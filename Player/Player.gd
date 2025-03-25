2290d8e8ca6b9bb35ef35012159c094cbc3fb98f
Nicholas Liu <liunicholas6@gmail.com>
2025-03-25 17:45:05 -0400

    Player fix (collision error + timestep independent movement)

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,37 +1,35 @@
 extends CharacterBody2D
 
-var pointer = position
+@export var pointer = position
+@export var speed : int
+@export var draw_range: int
+
 var target
-var speed = 100
-var distance = 100
-var draw_range = 150
+var distance
 
 func _ready():
 	target = position
 
-func in_range(pos):
-	if (pos-position).length() <= draw_range:
-		print(true)
-		return true
+#func in_range(pos):
+	#if (pos-position).length() <= draw_range:
+		#print(true)
+		#return true
 
 func _input(event):
 	if event.is_action_pressed("right_click"):
 		target = get_global_mouse_position()
-	if event.is_action_pressed("left_click"):
-		in_range(get_global_mouse_position())
+	#if event.is_action_pressed("left_click"):
+		#in_range(get_global_mouse_position())
 
 func _physics_process(delta):
 	pointer = get_global_mouse_position()
 	look_at(pointer)
-		
-	velocity = position.direction_to(target) * speed
 	
-	if position.distance_to(target)> 1:
-		set_velocity(velocity)
-		move_and_slide()
-	if get_slide_collision(0):
+	var target_velocity = position.direction_to(target) * speed * delta
+	velocity = target_velocity if position.distance_to(target) > 1 else Vector2()
+	
+	if move_and_slide():
 		target = position
-		velocity = Vector2(0,0)
 
 
 #func _physics_process(delta):
